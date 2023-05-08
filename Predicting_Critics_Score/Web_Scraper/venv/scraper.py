9d79f01a192a0a9fdba30c0300fa7712cec05c4c
reshma raghavan <u1430631@umail.utah.edu>
2023-05-07 21:09:13 -0600

    Separated imdb part

--- Predicting_Critics_Score/Web_Scraper/venv/scraper.py
+++ Predicting_Critics_Score/Web_Scraper/venv/scraper.py
@@ -8,7 +8,7 @@ config = {
   'user': 'rotten_scraper01',
   'password': 'n00b@THIS',
   'host': 'localhost',
-  'database': 'movie_info_rt',
+  'database': 'movie_info_collections',
   'raise_on_warnings': True
 }
 
@@ -19,7 +19,7 @@ url3 = "https://www.imdb.com/search/title/?groups=top_1000&sort=user_rating,desc
 url4 = "https://www.imdb.com/search/title/?groups=top_1000&sort=user_rating,desc&count=250&start=751"
 
 list_urls = [url1, url2, url3, url4]
-movie_names = []
+rotten_movie_names = []
 
 for list_url in list_urls:
     # Fetch the HTML content from the URL
@@ -29,33 +29,26 @@ for list_url in list_urls:
     # Parse the HTML content with BeautifulSoup
     list_soup = BeautifulSoup(list_html_content, 'html.parser')
 
-    # Find all `h3` tags with class `lister-item-header`
+    # Find all 'h3' tags with class 'lister-item-header'
     h3_tags = list_soup.find_all('h3', class_='lister-item-header')
 
-    # Loop through each `h3` tag and extract the movie title
+    # Loop through each 'h3' tag and extract the movie title
     for h3 in h3_tags:
-        a_tag = h3.find('a')  # Get the first `a` tag within the `h3`
-        if a_tag:  # Check if an `a` tag was found
-            # Extract the text within the `a` tag
+        a_tag = h3.find('a')  # Get the first 'a' tag within the 'h3'
+        if a_tag:  # Check if an 'a' tag was found
+            # Extract the text within the 'a' tag
             # and remove any leading/trailing whitespace
             movie_title = a_tag.text.strip()
-            movie_names.append(movie_title)  # Append the title to the list of movie names
+            rotten_movie_names.append(movie_title)  # Append the title to the list of movie names
 
-
-# Empty dictionary
-# movie_dictionary = {}
-
-# Empty list for storing movie dictionaries
-# movies = []
 special_characters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', ';', ':', "'", '"', '<', '>', ',', '.', '?', '/', '`', '~']
 formatted_title = ""
 
-for movie in movie_names:
+for movie in rotten_movie_names:
     for character in special_characters:
         # Remove unwanted characters from the movie title
         formatted_title = movie.replace(character, "").replace(" ", "_")
 
-    else:
         # Create the URL for the movie page
         movie_url = f"https://www.rottentomatoes.com/m/{formatted_title}"
 
@@ -69,16 +62,16 @@ for movie in movie_names:
 
             # Find the title tag and get its text content
             title_tag = movie_soup.find('title')
-            movie_title = title_tag.get_text()
+            rotten_movie_title = title_tag.get_text()
 
             # Remove the " - Rotten Tomatoes" suffix from the movie title
-            movie_title = movie_title.replace(" - Rotten Tomatoes", "")
+            rotten_movie_title = rotten_movie_title.replace(" - Rotten Tomatoes", "")
 
             # Find the <p> tag with the specified attributes and get its text content
-            synopsis = movie_soup.find('p', {'data-qa': 'movie-info-synopsis', 'slot': 'content'}).text
+            rotten_synopsis = movie_soup.find('p', {'data-qa': 'movie-info-synopsis', 'slot': 'content'}).text
 
             # Remove all newlines and other whitespace characters from the description
-            synopsis_short = synopsis.strip()
+            synopsis_short = rotten_synopsis.strip()
 
             # Find the score-details-manager section
             score_element = movie_soup.find('score-details-manager')
@@ -88,19 +81,19 @@ for movie in movie_names:
                 score_json = json.loads(score_element.find('script', {'id': 'score-details-json'}).text)
 
                 # Store the critics score (tomatometer score) in the variable
-                tomatometer_score = score_json['modal']['tomatometerScoreAll']['value']
+                rotten_score = score_json['modal']['tomatometerScoreAll']['value']
 
             except:
-                tomatometer_score = "N/A"
+                rotten_score = "N/A"
 
             try:
                 cnx = mysql.connector.connect(**config)
                 cursor = cnx.cursor()
 
                 add_movie = ("INSERT INTO movie "
-                             "(title, score, description) "
+                             "(movie_title, tomatometer_score, description) "
                              "VALUES (%s, %s, %s)")
-                movie_data = (movie_title, tomatometer_score, synopsis_short)
+                movie_data = (rotten_movie_title, rotten_score, synopsis_short)
 
                 # Insert new movie
                 cursor.execute(add_movie, movie_data)
@@ -123,5 +116,4 @@ for movie in movie_names:
         except:
             print(f"Failed to fetch data for movie {movie}")
 
-# print(movies)
-print("No. of movies stored: " + str(len(movies)))
+print("rotten tomatoes part done")
