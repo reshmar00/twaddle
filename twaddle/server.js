0210cd23384353fe61fbe056f4632134b852a548
reshma raghavan <u1430631@umail.utah.edu>
2023-09-16 09:27:12 -0600

    Added functionality for sending emails using client-side info

--- twaddle/server.js
+++ twaddle/server.js
@@ -1,45 +1,41 @@
 const express = require('express');
-const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const cors = require('cors');
+// Use CORS middleware
 app.use(cors());
-
 
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
 
+// Initialize Mailgun
+const formData = require('form-data');
+const Mailgun = require('mailgun-js');
+const mailgun = new Mailgun({
+    apiKey: 'b4249a24d01dd02a88b4040d0ea2a707-413e373c-810df927',
+    domain: 'sandbox62100d4345c548aaa13eff2b42166cc1.mailgun.org',
+});
+
 // Define a route for sending emails
 app.post('/send-email', (req, res) => {
-    const { email, subject, message } = req.body;
+    const { subject, message } = req.body; // Include password if needed for authentication
 
-    // Create a transporter for Gmail (or your email service provider)
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'somebodythatiusedtoknow269@gmail.com',
-            pass: 'simplepassword123!',
-        },
-    });
-
-    // Email data
-    const mailOptions = {
-        from: 'somebodythatiusedtoknow269@gmail.com',
-        to: 'somebodythatiusedtoknow269@gmail.com',
-        subject: "Here's a subject",
-        text: "Here is some text to go with the subject!",
+    const data = {
+        from: "Excited User <mailgun@sandbox62100d4345c548aaa13eff2b42166cc1.mailgun.org>",
+        to: ["shielabrof2004@yahoo.com"], // Update the recipient email address
+        subject: subject,
+        text: message,
+        html: `<h1>${message}</h1>`, // Use the message as HTML content
     };
 
-    // Send the email
-    transporter.sendMail(mailOptions, (error, info) => {
+    mailgun.messages().send(data, (error, body) => {
         if (error) {
             console.error('Error sending email:', error);
             res.status(500).json({ error: 'Error sending email' });
         } else {
-            console.log('Email sent:', info.response);
+            console.log('Email sent:', body);
             res.status(200).json({ message: 'Email sent successfully' });
         }
     });
@@ -49,4 +45,3 @@ app.post('/send-email', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
