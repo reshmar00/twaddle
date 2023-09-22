18d1191c8aed4b0ad5a3c45716f5091122ce1463
reshma raghavan <u1430631@umail.utah.edu>
2023-09-21 19:12:49 -0600

    Replaced stop button with pause button to match actual event functionality

--- twaddle/playSound.js
+++ twaddle/playSound.js
@@ -1,14 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     const playButton = document.querySelector('.play-button');
+    const themeAudio = document.getElementById('themeAudio');
+    let isPlaying = false;
 
-    // Function to handle play/pause behavior
+    // Function to handle play/pause behavior and button image
     const handlePlayPause = function () {
-        const themeAudio = document.getElementById('themeAudio');
-        if (themeAudio.paused) {
-            themeAudio.play();
-        } else {
+        if (isPlaying) {
             themeAudio.pause();
+            playButton.innerHTML = '<img src="res/play-button.png" alt="Play Icon">';
+        } else {
+            themeAudio.play();
+            playButton.innerHTML = '<img src="res/pause-button.png" alt="Stop Icon">';
         }
+        isPlaying = !isPlaying;
     }
 
     // Add an event listener to the play button
